ef333a8a77982b9476e9e1ff57f006c5caddcbfb
Amy Lipscomb <amylipscomb@amys-mbp.myfiosgateway.com>
2022-11-28 18:37:58 -0500

    changed the console.log message

--- db/connection.js
+++ db/connection.js
@@ -11,7 +11,7 @@ const connection = mysql.createConnection({
 
 connection.connect(function (err) {
   if (err) throw err;
-  console.log("It's connected!")
+  console.log("Connected!!!\n\n\n");
 });
 
 module.exports = connection;
