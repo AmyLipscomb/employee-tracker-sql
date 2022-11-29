852a6217a5149cf205d70dc9609313a220f89544
Amy Lipscomb <amylipscomb@macbook-pro.myfiosgateway.com>
2022-11-29 07:53:01 -0500

    created a main menu function

--- index.js
+++ index.js
@@ -1,3 +1,56 @@
-const connection = require('./db/connection.js');
+const db = require('./db/connection.js');
 const inquirer = require("inquirer");
+// const { listenerCount } = require('./db/connection.js');
 
+function mainMenu (){
+    inquirer.prompt([
+        {
+            type: "list",
+            name: "userchoice",
+            message: "What would you like to do?",
+            choices:["View all employees", "View all roles", "View all departments", "Add a department", "Add a role", "Add an employee", "Update an employee", "Quit"]
+
+
+        }
+    ])
+    .then((answers)=>{
+        switch(answers.userchoice){
+            case "View all employees":
+                viewAllEmployees()
+                break;
+            case "View all roles":
+                viewAllRoles()
+                break;
+            case "View all departments":
+                viewAllDepartments()
+                break;
+            case "Add a department":
+                addADepartment()
+                break;
+            case "Add a role":
+                addARole()
+                break;
+            case "Add an employee":
+                addAnEmployee()
+                break;
+            case "Update an employee":
+                updateAnEmployee()
+                break;
+            default: 
+                quit();
+        }
+    })
+
+}
+
+//continue to create the functions for all of the ones you made 
+
+function viewAllEmployees(){
+    
+}
+
+function quit(){
+    process.exit()
+}
+
+mainMenu()
