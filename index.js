79774b151f1d35f077b6379949acbb174cd7fcd7
Amy Lipscomb <amylipscomb@macbook-pro.myfiosgateway.com>
2022-11-29 19:48:22 -0500

    added a prepared statement to index.js

--- index.js
+++ index.js
@@ -1,6 +1,29 @@
 const db = require('./db/connection.js');
 const inquirer = require("inquirer");
-// const { listenerCount } = require('./db/connection.js');
+
+
+const viewAllEmployees = () => {
+    // (id, first_name, last_name, role_id, manager_id)
+
+   return db.query(
+        `CREATE VIEW employee SET ?`,
+        {
+            id: "1,2,3,4,5,6,7,8",
+            first_name: "John, Mike, Ashley, Kevin, Kunal, Malia, Sarah, Tom",
+            last_name: "Doe, Chan, Garcia, Tupik, Singh, Brown, Lourde, Holland",
+            role_id: "1,2,3,4",
+            manager_id: "1,2,3,4,NULL"
+        },
+        (err, result) => {
+            if (err) {
+              console.log(err);
+            }
+            console.log(result);
+          });
+
+   
+    return mainMenu();
+}
 
 function mainMenu (){
     inquirer.prompt([
@@ -45,24 +68,24 @@ function mainMenu (){
 
 //continue to create the functions for all of the ones made in the .then
 
-function viewAllEmployees(){
-    //enter a join
-}
+// function viewAllEmployees(){
+//     //enter a join
+// }
 
-function  viewAllRoles()
+// function  viewAllRoles()
 
-function viewAllDepartments()
+// function viewAllDepartments()
 
-function  addADepartment()
+// function  addADepartment()
 
-function  addARole()
+// function  addARole()
 
-function  addAnEmployee()
+// function  addAnEmployee()
 
-function  updateAnEmployee()
+// function  updateAnEmployee()
 
 function quit(){
     process.exit()
 }
 
-mainMenu()
+mainMenu();
